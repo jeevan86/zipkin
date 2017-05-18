6def5e80c50193938ec553c44c66619d1b0822ae
黄锏 <jeevan.huang@qq.com>
2017-05-18 10:38:23 +0800

    把collector-kafka10、autoconfigure-kafka10分离出来放到独立的module：zipkin-itm，并在这个module中放入zipkin-server的代码。

--- zipkin-itm/zipkin-itm/src/main/java/com/ffcs/itm/zipkin/busi/storage/cache/redis/IRedisUtils.java
+++
@@ -1,9 +0,0 @@
-package com.ffcs.itm.zipkin.busi.storage.cache.redis;
-
-import com.ffcs.itm.zipkin.busi.storage.cache.ICacheUtils;
-
-/**
- * Created by huangjian on 2017/3/22.
- */
-public interface IRedisUtils extends ICacheUtils {
-}
--- zipkin-itm/zipkin-itm/src/main/java/com/ffcs/itm/zipkin/busi/storage/cassandra/ICassandraUtils.java
+++
@@ -1,7 +0,0 @@
-package com.ffcs.itm.zipkin.busi.storage.cassandra;
-
-/**
- * Created by huangjian on 2017/5/17.
- */
-public class ICassandraUtils {
-}
